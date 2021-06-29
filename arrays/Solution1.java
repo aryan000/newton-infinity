f1d6b9506f5bab8475dc3fb23e328e31e90a212c
Aryan <aryandtu1@gmail.com>
2021-06-30 00:26:49 +0530

    First commit

---
+++ arrays/Solution1.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Assignment1 -> Printing Hello World
+
+class Solution1 {
+
+	public static void main(String[] args){
+
+		System.out.println("Hello World");
+	}
+}
